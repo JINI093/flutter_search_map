ada7535121cba4a5ca5efa7e92b49b6a329f05ad
JINI093 <koubook.jin@gamil.com>
2024-12-09 15:30:05 +0900

    model내부 외부 Api 불러오기 추가

---
+++ lib/model/map.dart
@@ -0,0 +1,62 @@
+// {
+//       "title": "커피해리902",
+//       "link": "https://www.instagram.com/coffeeharry902",
+//       "category": "음식점>카페,디저트",
+//       "description": "",
+//       "telephone": "",
+//       "address": "인천광역시 서구 원당동 1030-8 검단퍼스트 112호",
+//       "roadAddress": "인천광역시 서구 이음5로 80 검단퍼스트 112호",
+//       "mapx": "1267163311",
+//       "mapy": "375953218"
+//     }
+
+class Guidance {
+  final String title;
+  final String link;
+  final String category;
+  final String description;
+  final String telephone;
+  final String address;
+  final String roadAddress;
+  final String mapx;
+  final String mapy;
+
+  Guidance({
+    required this.title,
+    required this.link,
+    required this.category,
+    required this.description,
+    required this.telephone,
+    required this.address,
+    required this.roadAddress,
+    required this.mapx,
+    required this.mapy,
+  });
+
+  Guidance.fromJson(Map<String, dynamic> map)
+      : this(
+          title: map['title'],
+          link: map['link'],
+          category: map['category'],
+          description: map['description'],
+          telephone: map['telephone'],
+          address: map['address'],
+          roadAddress: map['roadAddress'],
+          mapx: map['mapx'],
+          mapy: map['mapy'],
+        );
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'link': link,
+      'category': category,
+      'description': description,
+      'telephone': telephone,
+      'address': address,
+      'roadAddress': roadAddress,
+      'mapx': mapx,
+      'mapy': mapy,
+    };
+  }
+}
